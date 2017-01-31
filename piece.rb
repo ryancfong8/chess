ba810b8f58d27719396c86f53f3bd90d0c415b90
App Academy Student <student@appacademy.io>
2017-01-30 17:55:38 -0800

    basic outline for piece logic

--- piece.rb
+++ piece.rb
@@ -1,20 +1,68 @@
 class Piece
   attr_reader :color, :type
+  include SlidingPiece
+  include SteppingPiece
   def initialize(type, color)
     @type = type
     @color = color
   end
+
+  def moves
+    # return an array of places a piece can move to
+  end
+
+  def move_dirs
+    # for bishop, rook, queen moves
+  end
 end
 
-class SlidingPiece < Piece
-  # bishop, rook, queen
-end
+class Pawns < Piece
+  def initialize(type, color)
+  end
 
-class SteppingPiece < Piece
+  def moves
+    # can move up, down, right, left, diagonally
+    # [+1,0]
+    # can do these moves if attacking
+    # [+1,+1], [+1,-1]
+  end
 end
 
 class NullPiece < Piece
   def initialize
     @type = nil
+  end
+end
+
+module SlidingPiece
+  def initialize(type, color)
+  end
+
+  def moves
+    #bishop can only go diagonally, as many spaces as possible
+    # [+n,+n], [-n,-n], [-n, +n], [+n, -n]
+
+    #rook can go up, down, right left as many spaces as possible
+    # [+n, 0], [-n, 0], [0, +n], [0, -n]
+
+    # queen can go anywhere to the max
+    # [+n,+n], [-n,-n], [-n, +n], [+n, -n], [+n, 0], [-n, 0], [0, +n], [0, -n]
+
+  end
+end
+
+module SteppingPiece
+  def initialize(type, color)
+  end
+
+  def moves
+    # king can move one space in any direction:
+      # [+1, 0], [+1, +1], [0,+1], [+1,-1], [0, -1], [-1,0], [-1,-1], [-1,+1]
+
+    # knight has 4 options:
+      # current pos [+2, +1]
+      # current pos [+2, -1]
+      # current pos [-2, +1]
+      # current pos [-2, -1]
   end
 end
